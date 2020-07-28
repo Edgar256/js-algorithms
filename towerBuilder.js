31afe14757aacb41d1e01d5c74347139546d04d5
EDGART <edgar@getpoints.com>
2020-07-28 19:33:03 +0300

    added descriotion for truncateString module

--- towerBuilder.js
+++ towerBuilder.js
@@ -7,7 +7,6 @@ function towerBuilder(nFloors) {
     let temp = 0,rArr = []
     for(let i = 0; i < arr.length; i++){
         temp = (bLength-arr[i].length)/2;
-        // console.log(temp)
         let emptySpace = ' ',curIndex;
         if(i !== arr.length-1){
             curIndex = emptySpace.repeat(temp) + arr[i] + emptySpace.repeat(temp)
--- truncateString.js
+++ truncateString.js
@@ -1,5 +1,19 @@
+/*
+  Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+  Return the truncated string with a … ending.
+  Note that inserting the three dots to the end will add to the string length
+
+  However, if the given maximum string length num is less than or equal to 3, then the addition of the three
+  dots does not add to the string length in determining the truncated string.
+
+  Examples:
+  truncateString('Edgar',3) should return -> 'Edg...'
+  truncateString('A-tisket a-tasket A green and yellow basket', 8) should return -> 'A-tisket...'
+  
+*/
+
 function truncateString(str, num) {
-    if(!str || !num) return null;
+    if(!str || !num) return null; // return null for empty inputs
     if(num < str.length){
         let trunc = (str.slice(0,num)).concat('...');
         return trunc;
